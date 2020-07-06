34d91d82342ef3efd02448e1fce8ba4d5cfe902f
AlexisAle <alexistuc@yahoo.com>
2020-07-06 10:49:59 -0300

    tp 9 alexis ale

---
+++ AlexisAleTrabajoPractico9TallerDeLenguajes/Helpers.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+using System.Linq;
+
+namespace AlexisAleTrabajoPractico9TallerDeLenguajes
+{
+    static class SoporteParaConfiguracion
+    {
+        static public void CrearArchivoDeConfiguracion(string path)
+        {
+            if (!File.Exists(path))
+            {
+                string NuevoArchivo = "destino.dat";
+
+                if (!File.Exists(NuevoArchivo))
+                {
+                    File.Create(NuevoArchivo);
+                }
+                File.WriteAllText(NuevoArchivo, path);
+            }
+            else
+            {
+                Console.WriteLine("Archivo Existente");
+            }
+        }
+        
+        public static string LeerConfiguracion(string Destino)
+        {
+            BinaryReader LeerBinario = new BinaryReader(File.Open(Destino + @"\destino.dat", FileMode.Open));
+            string contenido = LeerBinario.ReadString();
+            LeerBinario.Close();
+            return contenido;
+        }
+
+        public static class ConversorDeMorse
+        {
+            public static Dictionary<char, string> Diccionario = new Dictionary<char, string>
+            {
+                {'a', ".-"},
+                {'b', "-..."},
+                {'c', "-.-."},
+                {'d', "-.."},
+                {'e', "."},
+                {'f', "..-."},
+                {'g', "--."},
+                {'h', "...."},
+                {'i', ".."},
+                {'j', ".---"},
+                {'k', "-.-"},
+                {'l', ".-.."},
+                {'m', "--"},
+                {'n', "-."},
+                {'o', "---"},
+                {'p', ".--."},
+                {'q', "--.-"},
+                {'r', ".-."},
+                {'s', "..."},
+                {'t', "-"},
+                {'u', "..-"},
+                {'v', "...-"},
+                {'w', ".--"},
+                {'x', "-..-"},
+                {'y', "-.--"},
+                {'z', "--.."},
+                {'0', "-----"},
+                {'1', ".----"},
+                {'2', "..---"},
+                {'3', "...--"},
+                {'4', "....-"},
+                {'5', "....."},
+                {'6', "-...."},
+                {'7', "--..."},
+                {'8', "---.."},
+                {'9', "----."},
+                {' ', "\\" },
+            };
+            
+            static public string TextoAMorse(string cadena)
+            {
+                string traduccion = "";
+
+                cadena = cadena.ToLower();
+
+                foreach (char letra in cadena)
+                {
+                    foreach (KeyValuePair<char, string> elemento in Diccionario)
+                    {
+                        if (Convert.ToChar(elemento.Key) == letra)
+                        {
+                            traduccion = traduccion + elemento.Value;
+                            break;
+                        }
+                    }
+                }
+                Console.WriteLine("TRADUCIDO A CODIGO MORSE:");
+                Console.WriteLine(traduccion);
+                return traduccion;
+            }
+
+            
+        }
+    }
+}
---
+++ AlexisAleTrabajoPractico9TallerDeLenguajes/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using static AlexisAleTrabajoPractico9TallerDeLenguajes.SoporteParaConfiguracion;
+
+namespace AlexisAleTrabajoPractico9TallerDeLenguajes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ruta = Directory.GetCurrentDirectory();
+            //string ruta = @"C:\Users\usuario\Documents\myPenDrive\Taller de Lenguaje I\2020\trabajo practico 9\tpn9 - alexistuc\AlexisAleTrabajoPractico9TallerDeLenguajes";
+            SoporteParaConfiguracion.CrearArchivoDeConfiguracion(ruta);
+            string DataDirectory = SoporteParaConfiguracion.LeerConfiguracion(ruta);
+
+            //string fecha = DateTime.Now.ToString("dd-MM-yy");
+            /*if (!Directory.Exists(ruta))
+            {
+                Directory.CreateDirectory(ruta);
+            }
+            */
+            if (!Directory.Exists(ruta))
+            {
+                Directory.CreateDirectory(ruta + @"Morse\");
+            }
+            Console.WriteLine("Segun el archivo de configuracion se creo un directorio llamado " + DataDirectory + ", en el cual se guardaran todos los archivos\n");
+
+            Console.Write("Ingrese una frase para traducirla a morse: ");
+            string aTraducir = Console.ReadLine();
+
+
+            //TEXTO A MORSE
+            string TextToMorse = ConversorDeMorse.TextoAMorse(aTraducir.ToUpper());
+            //string fecha = ConversorDeMorse.TextoAMorse(TextToMorse, DataDirectory);
+            //ConversorDeMorse.MorseToMp3(TextToMorse, DataDirectory, fecha);
+
+            //*********************** MORSE A TEXTO ***********************//
+            //ConversorDeMorse.CrearTextoTxt(DataDirectory, fecha);
+
+
+            Console.WriteLine("\nLas corrspondientes traducciones se guardaron dentro del directorio " + DataDirectory);
+
+            // TODO: Implement Functionality Here
+
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
